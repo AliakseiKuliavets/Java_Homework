a86976077c317b66b1f75401b2e70f2ccba5c716
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-22 14:04:00 +0200

    Add files via upload

--- Lesson1/Lesson1.java
+++ Lesson1/Lesson1.java
@@ -1,5 +1,26 @@
+// 1. В методе main инициализировать все примитивные типы и не примитивные типы.
+// 2. Вывести их результат в консоль:
+/*char: G
+        int: 89
+        byte: 4
+        short: 56
+        float: 4.7333436
+        double: 4.355453532
+        long: 12121
+
+        Character: G
+        Integer: 89
+        Byte: 4
+        Short: 56
+        Float: 4.7333436
+        Double: 4.355453532
+        Long: 12121
+*/
+
+
 public class Lesson1 {
     public static void main(String[] args) {
+
         char myChar = 'G';
         int myInt = 89;
         byte myByte = 4;
@@ -8,6 +29,11 @@ public class Lesson1 {
         double myDouble = 4.355453532;
         long myLong = 12121;
 
+        boolean myBool = true;
+        String myString = "String";
+        String[] array = {"first", "second"};
+
+
         System.out.println("char: "+myChar);
         System.out.println("int: "+myInt);
         System.out.println("byte: "+myByte);
@@ -15,7 +41,9 @@ public class Lesson1 {
         System.out.println("float: "+myFloat);
         System.out.println("double: "+myDouble);
         System.out.println("long: "+myLong);
+
         System.out.println();
+
         System.out.println("Character: "+myChar);
         System.out.println("Integer: "+myInt);
         System.out.println("Byte: "+myByte);
@@ -23,6 +51,5 @@ public class Lesson1 {
         System.out.println("Float: "+myFloat);
         System.out.println("Double: "+myDouble);
         System.out.println("Long: "+myLong);
-
     }
 }
--- Lesson1/Lesson1Plus.java
+++ Lesson1/Lesson1Plus.java
@@ -1,11 +1,28 @@
+// 1. Дано трехзначное число. Вывести на экран все цифры этого числа
+/* Пример: 345
+   Вывод в консоль: Число 345 -> 3, 4, 5
+
+   Дальше я сам уже добавил:
+   запрашивать ввод ,
+   проверять какое число если меньше 100 или больше 999 то не работать с ним
+*/
+
+
+import java.util.Scanner;
+
 public class Lesson1Plus {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число от 100 до 999");
+        int number = sc.nextInt();
+        if ( number >=100 && number <= 999) {
+            int num1 = number % 10;
+            int num2 = ((number % 100) - num1) / 10 ;
+            int num3 = (number -((num2 * 10) +num1 )) / 100;
 
-        int number = 345;
-        int num1 = number % 10;
-        int num2 = ((number % 100) - num1) / 10 ;
-        int num3 = (number -((num2 * 10) +num1 )) / 100;
-
-        System.out.println(num3 + ", " + num2 + ", " + num1);
+            System.out.println("Число "+number+" -> " + num3 + ", " + num2 + ", " + num1);
+        } else {
+            System.out.println("Введено число меньше 100 или больше 999");
+        }
     }
 }
