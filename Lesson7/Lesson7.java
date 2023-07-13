d401136e2246a669789cfe42d1304d6daa2d5e25
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-07-13 13:52:22 +0200

    Add files via upload

---
+++ Lesson7/Lesson7.java
@@ -0,0 +1,29 @@
+/*
+№1.
+
+        Создайте массив из 8 случайных целых чисел из интервала [1;50]
+        Выведите массив на консоль в строку.
+        Замените каждый элемент с нечетным индексом на ноль.
+        Снова выведете массив на консоль в отдельной строке.
+*/
+
+
+
+import java.util.Random;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+        int[] arrays = new int[8];
+        Random random = new Random();
+        for (int val = 0; val < arrays.length; val++)
+            arrays[val] = random.nextInt(50);
+        for (int val : arrays) System.out.print(val + " ");
+        System.out.println();
+        for (int val : arrays) {
+            if (val % 2 != 0) {
+                val = 0;
+            }
+        System.out.print(val + " ");
+        }
+    }
+}
---
+++ Lesson7/Lesson72.java
@@ -0,0 +1,47 @@
+/*
+№2.
+
+        Создайте массив из 5 случайных целых чисел из интервала [10;99]
+        Выведите его на консоль в строку.
+        Определите и выведите на экран сообщение о том, является ли массив строго возрастающей последовательностью.
+*/
+
+
+
+
+import java.util.Random;
+
+public class Lesson72 {
+    public static void main(String[] args) {
+        int[] arrays = new int[5];
+        Random random = new Random();
+        for (int val = 0; val < arrays.length; val++)
+            arrays[val] = random.nextInt(90)+10;
+
+        // для проверки (часто он не является возращающей и можно вручную загнать значения)
+//        arrays[0] = 2;
+//        arrays[1] = 3;
+//        arrays[2] = 4;
+//        arrays[3] = 5;
+//        arrays[4] = 6;
+
+        noSort(arrays);
+        System.out.println();
+        System.out.println((sortOrNo(arrays) == true ? "Массив является строго возрастающей последовательностью. " : "")
+                + (sortOrNo(arrays) == false  ? "Массив не является строго возрастающей последовательностью." : ""));
+    }
+    static boolean sortOrNo(int[] arrays) {
+        boolean sort = true;
+        for (int i = 0; i < arrays.length - 1; i++) {
+            if (arrays[i] >= arrays[i + 1]) {
+                sort = false;
+                return sort;
+            }
+        }
+                return sort;
+
+    }
+    static void noSort(int[] arrays) {
+        for (int val : arrays) System.out.print(val + " ");
+    }
+}
