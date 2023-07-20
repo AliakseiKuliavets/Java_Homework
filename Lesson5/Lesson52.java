87ef2d7ee301d2369cb3a2800e8cfec962ed2fdc
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-07-20 18:54:41 +0200

    Update Lesson52.java

--- Lesson5/Lesson52.java
+++ Lesson5/Lesson52.java
@@ -2,27 +2,27 @@
         Реализуйте программу, которая попросит пользователя ввести год
          и напечатать этот год isLeap (високосный) или нет.*/
 
+package org.telran.lesson15New;
+
 import java.util.Scanner;
 
-public class Lesson52 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите какой год начиная c 1584: ");
-        int year = scanner.nextInt();
-        if (year < 1584){
-            System.out.println("Я же просил не меньше 1584");
-        } else {
-            System.out.println(year4(year));
-        }
+    public class Lesson52 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите какой год начиная c 1584: ");
+            int year = scanner.nextInt();
+            if (year < 1584){
+                System.out.println("Я же просил не меньше 1584");
+            } else {
+                year4(year);
+            }
 
-    }
-    static String year4(int year) {
-        String str = "";
-        if ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            str = "Год выскокосный";
-        } else {
-            str = "Год не высокосный";
         }
-        return str;
+        static void year4(int year) {
+            if ((( year % 4 == 0) && (year % 100 != 0 ))|| year % 400 == 0) {
+                System.out.println("Год высокосный");
+            } else  {
+                System.out.println("Не высокосный");
+            }
+        }
     }
-}
